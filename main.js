100ae99609ec281f420c774264f8c33d6d90f3c7
Eng Sayed Abdo <109088241+Sayed3bdu@users.noreply.github.com>
2022-07-13 14:03:13 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+/*
+    data types intro
+    string
+    number
+    array=> object
+    object
+    boolen
+______________________________
+console.log("sayed abdo");
+console.log(typeof"sayed abdo");
+console.log(typeof 5000);
+console.log(typeof 5000.67);
+console.log(typeof [10, 13, 30]);
+console.log(typeof["sa", "ab"]);
+console.log(typeof {name:'sayed'})
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof undefined);
+console.log(typeof null);
+*/
+/*
+    variables intro
+    -
+__________________________
+var user = "Abdo"
+console.log(user);
+console.log("sayed");
+console.log("sayed");
+console.log("sayed");
+console.log(hello);
+hello.innerHTML = "asd"
+*/
+/*
+    Identifiers
+    - name conventions and rules
+    - reserved words
+
+
+let a = "we love"
+console.log(`${a} 
+${a}`);
+
+let d ="-100";
+let e ="20";
+let f = 30;
+let g = true;
+
+console.log(++e * ++g + -d + ++f);
+console.log(-d*--e)
+
+let a = 10;
+let b = "20";
+let c = 80;
+
+console.log(++a + b++ + c++ ) 
+console.log(a + b + c)
+*/
